3d060e0e15de29b45406ca8a28ce71ca7cd58939
shiguma127 <shiguma127@gmail.com>
2021-11-16 14:42:53 +0900

    add:theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React from "react";
+import { CssBaseline, ThemeOptions } from "@mui/material";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const themeOptions: ThemeOptions = {
+    palette: {
+      primary: {
+        main: "#1976d2",
+      },
+      secondary: {
+        main: "#f50057",
+      },
+      background: {
+        default: "#f2f5f9",
+      },
+    },
+  };
+  const theme = createTheme(themeOptions);
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Component {...pageProps} />
+      </CssBaseline>
+    </ThemeProvider>
+  );
 }
 export default MyApp;
