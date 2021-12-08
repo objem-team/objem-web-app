1b78752d498a81d48bf3da8e81860c71604948fd
aozki <kcska_1818@kcska.onmicrosoft.com>
2021-12-08 13:37:45 +0900

    トグル・スイッチ追加しました

--- components/AppDrawer.tsx
+++ components/AppDrawer.tsx
@@ -1,46 +1,66 @@
 import React from "react";
-import MailIcon from "@mui/icons-material/Mail";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import {
-  Divider,
-  Drawer,
-  DrawerProps,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+
+import { Drawer, DrawerProps, List } from "@mui/material";
+import Button from "@mui/material/Button";
+import Switch from "@mui/material/Switch";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 
 const AppDrawer: React.VFC<DrawerProps> = (props) => {
+  const [widgetalignment, setWidgetAlignment] = React.useState("unlock");
+  const [alignment, setAlignment] = React.useState("light");
+
+  const widgetHandleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string
+  ) => {
+    if (newAlignment !== null) {
+      setWidgetAlignment(newAlignment);
+    }
+  };
+
+  const themeHandleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string
+  ) => {
+    if (newAlignment !== null) {
+      setAlignment(newAlignment);
+    }
+  };
+
   return (
     <Drawer open={props.open} onClose={props.onClose}>
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem
-            button
-            key={text}
-            sx={{
-              width: "15vw",
-            }}
-          >
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+      <List sx={{ mx: 3, mt: 1 }}>
+        <label>ウィジェットのロック</label>
+        <Switch sx={{ ml: 4 }} />
       </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+
+      <List sx={{ mx: 3 }}>
+        <label>テーマ</label>
+        <ToggleButtonGroup
+          color="primary"
+          value={alignment}
+          exclusive
+          onChange={themeHandleChange}
+          size="small"
+          fullWidth
+        >
+          <ToggleButton value="light">
+            <LightModeIcon />
+            ライト
+          </ToggleButton>
+          <ToggleButton value="dark">
+            <DarkModeIcon />
+            ダーク
+          </ToggleButton>
+        </ToggleButtonGroup>
       </List>
+
+      <Button variant="outlined" sx={{ mx: 3, my: 6 }}>
+        dissconnect
+      </Button>
     </Drawer>
   );
 };
