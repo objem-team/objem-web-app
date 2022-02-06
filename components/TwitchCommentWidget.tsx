593528d60868c9592f38706817efdafbceae34ff
shiguma127 <shiguma127@gmail.com>
2022-02-07 00:33:51 +0900

    add:ObjemDesktopから配信IDの取得

--- components/TwitchCommentWidget.tsx
+++ components/TwitchCommentWidget.tsx
@@ -1,20 +1,58 @@
 import * as React from "react";
+import { WebsocketMessage } from "../src/types/WebsocketMessage";
+import { websocket as ws } from "../src/websocket";
 import IframeComponent from "./IframeComponent";
+import { Skeleton } from "@mui/material";
 
-type TwitchCommentWidgetProps = {
-  videoId: string;
-};
+const TwitchCommentWidget: React.VFC = () => {
+  const [visible, setVisible] = React.useState(true);
+  const [videoId, setVideoId] = React.useState<string>("");
+  React.useEffect(() => {
+    const connection = ws.getInstance().connection;
+    connection.addEventListener("message", onMassage);
+    if (connection.readyState > 0) {
+      requestStreamURL();
+    } else {
+      connection.addEventListener("open", requestStreamURL);
+    }
+    return () => {
+      connection.removeEventListener("message", onMassage);
+      connection.removeEventListener("open", requestStreamURL);
+    };
+  }, []);
 
-const TwitchCommentWidget: React.VFC<TwitchCommentWidgetProps> = (props) => {
+  const onMassage = (event: MessageEvent) => {
+    const data = JSON.parse(event.data) as WebsocketMessage;
+    switch (data.eventName) {
+      case "TwitchVideoId":
+        const videoId = data.data;
+        if (!videoId) setVisible(false);
+        setVideoId(videoId);
+        break;
+    }
+  };
+  const requestStreamURL = () => {
+    const connection = ws.getInstance().connection;
+    const request: WebsocketMessage = {
+      eventName: "requestTwitchVideoId",
+      data: "this is requestShortcuts",
+    };
+    console.log("send");
+    connection.send(JSON.stringify(request));
+  };
   return (
-    <IframeComponent
-      src={
-        "https://www.twitch.tv/embed/" +
-        props.videoId +
-        "/chat?parent=localhost"
-      }
-      height={400}
-    />
+    <>
+      {visible && videoId ? (
+        <IframeComponent
+          src={
+            "https://www.twitch.tv/embed/" + videoId + "/chat?parent=localhost"
+          }
+          height={400}
+        />
+      ) : (
+        <Skeleton variant="rectangular" width={"90vw"} height={400} />
+      )}
+    </>
   );
 };
 
--- components/YouTubeCommentWidget.tsx
+++ components/YouTubeCommentWidget.tsx
@@ -1,19 +1,60 @@
 import * as React from "react";
+import { WebsocketMessage } from "../src/types/WebsocketMessage";
+import { websocket as ws } from "../src/websocket";
 import IframeComponent from "./IframeComponent";
-type YouTubeCommentWidgetProps = {
-  videoId: string;
-};
+import { Skeleton } from "@mui/material";
 
-const YouTubeCommentWidget: React.VFC<YouTubeCommentWidgetProps> = (props) => {
+const YouTubeCommentWidget: React.VFC = () => {
+  const [visible, setVisible] = React.useState(true);
+  const [videoId, setVideoId] = React.useState<string>("");
+  React.useEffect(() => {
+    const connection = ws.getInstance().connection;
+    connection.addEventListener("message", onMassage);
+    if (connection.readyState > 0) {
+      requestStreamURL();
+    } else {
+      connection.addEventListener("open", requestStreamURL);
+    }
+    return () => {
+      connection.removeEventListener("message", onMassage);
+      connection.removeEventListener("open", requestStreamURL);
+    };
+  }, []);
+
+  const onMassage = (event: MessageEvent) => {
+    const data = JSON.parse(event.data) as WebsocketMessage;
+    switch (data.eventName) {
+      case "YouTubeVideoId":
+        const videoId = data.data;
+        console.log(videoId);
+        if (!videoId) setVisible(false);
+        setVideoId(videoId);
+        break;
+    }
+  };
+  const requestStreamURL = () => {
+    const connection = ws.getInstance().connection;
+    const request: WebsocketMessage = {
+      eventName: "requestYoutubeVideoId",
+      data: "this is requestYoutubeVideoId",
+    };
+    connection.send(JSON.stringify(request));
+  };
   return (
-    <IframeComponent
-      src={
-        "https://www.youtube.com/live_chat?v=" +
-        props.videoId +
-        "&embed_domain=localhost"
-      }
-      height={400}
-    />
+    <>
+      {visible && videoId ? (
+        <IframeComponent
+          src={
+            "https://www.youtube.com/live_chat?v=" +
+            videoId +
+            "&embed_domain=localhost"
+          }
+          height={400}
+        />
+      ) : (
+        <Skeleton variant="rectangular" width={"90vw"} height={400} />
+      )}
+    </>
   );
 };
 export default YouTubeCommentWidget;
